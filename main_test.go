6e05d7883128b0957265f7cb444571302c9ed89b
novian kristianto <kristianto.novian@gmail.com>
2019-09-18 02:24:46 +0700

    update main

--- main_test.go
+++ main_test.go
@@ -24,7 +24,7 @@ func TestHandler(t *testing.T) {
 			status, http.StatusOK)
 	}
 
-	expected := fmt.Sprintf("Hi there, my name is %s", testValue)
+	expected := fmt.Sprintf("Hi there, my name is %s, nice to meet you!", testValue)
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
