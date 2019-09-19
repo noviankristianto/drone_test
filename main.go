10b73a43ffd32e201afb3a958dd2badf7d16bdc3
Randy Cahyana <ranren.cahyana@gmail.com>
2019-09-19 18:32:31 +0700

    i am YesVian

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"net/http"
 )
 
+const resp = "Hi (not novian here), my name is %s"
+
 func main() {
 	http.HandleFunc("/", greetingHandler)
 	log.Fatal(http.ListenAndServe(":8989", nil))
 }
 
 func greetingHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, my name is %s", r.URL.Path[1:])
+	fmt.Fprintf(w, resp, r.URL.Path[1:])
 }
